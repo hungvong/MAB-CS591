98f4156fe271101a6599a813fbb6fa4a05157903
Courtney Pacheco <musician@bu.edu>
2015-04-11 14:50:35 -0400

    Mechanism 1 now works. Woooo

--- Mechanism1.py
+++ Mechanism1.py
@@ -4,14 +4,6 @@ from math import log10
 #Set number of agents
 n = 10
 
-#Delta function
-d = (k**(-1/3)) * (log10(n))**(2/3)
-
-#Compute active prices P
-i_len = 10
-P = np.zeros(1,i_len)
-for i in range (1,i_len):
-	P[i] = d*(1+d)^i 
 
 #Settings
 t = 0
@@ -19,6 +11,16 @@ p = 10 #Depends on the MAB
 Nt = {}
 kt = np.array([1,1,4,0,1]) #depends on the MAB
 k = np.sum(kt) #number of items left
+alpha = np.floor(log10(n))
+
+#Delta function
+d = (k**(-1/3)) * (log10(n))**(2/3)
+
+#Compute active prices P
+i_len = 10
+P = np.zeros((i_len))
+for i in range (1,i_len):
+	P[i] = d*(1+d)**i 
 
 #Initialize arrays
 St = []
@@ -34,8 +36,8 @@ while (k >= 1):
 		Nt[p] += 1;
 
 	St.extend([kt[t] / Nt[p]])
-	rt.extend(alpha / (Nt[p] + 1) + np.sqrt((alpha*St[t])/(Nt[p]+1)))
-	It.extend(p * np.min(k, n*(St[t] + rt[t])))
+	rt.extend([(alpha / (Nt[p] + 1)) + np.sqrt((alpha*St[t])/(Nt[p]+1))])
+	It.extend([p * np.min((k, n*(St[t] + rt[t])))])
 	p = np.argmax(It[t])
 	k = k - kt[t]
 
