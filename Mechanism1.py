5d0d4f9335be4f41839f1b5a27d1125e9647c636
Courtney Pacheco <musician@bu.edu>
2015-04-12 00:02:59 -0400

    Fixed Mechanism1.py

--- Mechanism1.py
+++ Mechanism1.py
@@ -2,58 +2,78 @@ import numpy as np
 from math import log10
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+import random
 
-#Set number of agents
-n = 30
+k = 100. #Number of items
+n = 30. #number of buyers
+i = 10 #total number of prices in list
+Nt = {} #Dictionary which holds the number of times a price has occurred in a round
+t = 0 #Stores current round
+p_list = []
 
-#Settings
-t = 0
-p = 1 #Depends on the MAB
-p_list = [] #Saves history of prices, per round
-Nt = {}
-kt = np.array([6,3,8,9,11,2,1,100,50,33,2,8,19,88])
-#kt = np.array([0,1,4,0,1,0,15,30,10,2]) #depends on the MAB
-k = np.sum(kt) #number of items left
-alpha = np.floor(log10(n))
+#compute delta
+delta = (k**(-1./3.)) * ((log10(n))**(2./3.))
 
-#Delta function
-d = (k**(-1/3)) * (log10(n))**(2/3)
+#compute alpha
+alpha = np.floor(log10(float(n)))
 
-#Compute active prices P
-i_len = 10
-P = np.zeros((i_len))
-for i in range (1,i_len):
-	P[i] = d*(1+d)**i 
+#Initialize list of prices & dictionary
+P = np.zeros(i)
+for jj in range (1, i+1):
+	P[jj-1] = delta * (1 + delta)**jj
+	Nt[P[jj-1]] = 0;
 
-#Initialize lists
-St = []
-rt = []
-It = []
+#Initialize rt, St
+rt = {}
+St = {}
+
+#input kt dictionary
+kt = dict()
+for price in P:
+	kt[price] = random.randint(0,20)
 
 while (k >= 1):
 
+	It_vals = []
+
+	#Find which price minimizes It
+	for jj in range (0,i):
+		p = P[jj]
+
+		#Compute St[p]
+		try:
+			St[p] = float(kt[p]) / Nt[p]
+		except ZeroDivisionError:
+			St[p] = 1
+
+		#Compute rt[p]
+		rt[p] = (alpha / (Nt[p] + 1.)) + np.sqrt( (alpha * St[p]) / (Nt[p] + 1.) )
+
+		#Compute It
+		It = p * np.min((k, n*(St[p] + rt[p])))
+
+		It_vals.extend([It])
+
+	idx = np.argmax(It_vals)
+	p = P[idx]
 	p_list.extend([p])
 
-	#Update the Nt dictionary
-	if (p not in Nt):
-		Nt[p] = 1; #This is the first time p has occurred in this auction
-	else:
-		Nt[p] += 1;
+	#Add chosen price to Nt
+	Nt[p] += 1
 
-	St.extend([ float(kt[t]) / float(Nt[p])])
-	rt.extend([( float(alpha) / float((Nt[p] + 1))) + np.sqrt((float(alpha*St[t]))/float((Nt[p]+1)))])
-	It.extend([p * np.min((k, n*(St[t] + rt[t])))])
-	p = np.argmax(It) + 1
-	k = k - kt[t]
-
-	#Increment the current round number
 	t += 1
+
+	k -= kt[p]
+
+kt_df = pd.DataFrame(kt.items(),columns=['Price','N_sold'])
+kt_df = kt_df.sort(['Price'])
 
 #Plot the results
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-t_vector = np.arange(t) + 1
-ax.plot(t_vector, p_list, kt)
+t_vector = np.arange(len(P))+1#np.arange(t) + 1
+ax.plot(t_vector, P, kt_df['N_sold'])
 ax.set_xlabel('Round')
 ax.set_ylabel('Price')
 ax.set_zlabel('Number of items sold')
